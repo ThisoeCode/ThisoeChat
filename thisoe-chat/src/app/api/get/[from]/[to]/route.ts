e1d9d2fabdac7a9d66cf0dbee30a0b856ef79f32
Thisoe <thisoecode@gmail.com>
2025-02-02 18:43:13 +0900

    [1.0.3] SEO: Added obots.txt; fixed type errors

--- thisoe-chat/src/app/api/get/[from]/[to]/route.ts
+++ thisoe-chat/src/app/api/get/[from]/[to]/route.ts
@@ -2,12 +2,11 @@ import{NJ}from"@/lib/logsys"
 import{auth}from"@/lib/auth"
 import{retrieveHistory}from"@/lib/server"
 import type{NextRequest}from"next/server"
-import type{awaitable}from"@/lib/ts"
 
 export const
 GET=async(
   _:NextRequest,
-  {params}:{params:awaitable<{from:string,to:string}>}
+  {params}:{params:Promise<{from:string,to:string}>}
 )=>{return auth(async(req)=>{if(req.auth?.user){
   return NJ({ok:1,chats:retrieveHistory(params)})
 } return NJ({ok:0},401)
